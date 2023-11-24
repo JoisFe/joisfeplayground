725590bc27cec15c91915060e0a3a971ca872e34
JoisFe <rhsnqk@gmail.com>
2023-11-24 20:28:50 +0900

    josifeplayground project start

---
+++ src/main/java/com/joisfe/joisfeplayground/JoisfeplaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.joisfe.joisfeplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoisfeplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoisfeplaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joisfe/joisfeplayground/JoisfeplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joisfe.joisfeplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoisfeplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
